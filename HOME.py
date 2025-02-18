c797ea8d4440d5fc0ae8d17f2eaae9d8a7acd251
adityavardhan1 <30662829+adityavardhan1@users.noreply.github.com>
2025-02-18 14:02:29 +0530

    v2

--- HOME.py
+++ HOME.py
@@ -8,53 +8,14 @@ def load_css(file_name):
 
 load_css("structure.css")
 
-
-st.markdown("""
-    <style>
-        /* Hide the Deploy button */
-        header [data-testid="stDeployButton"] {
-            display: none !important;
-        }
-        
-        /* Hide Streamlit footer */
-        footer {visibility: hidden;}
-    </style>
-""", unsafe_allow_html=True)
-
-st.markdown("""
-    <style>
-        .top-right-menu {
-            position: absolute;
-            top: 10px;
-            right: 20px;
-            z-index: 1000;
-            background-color: #f8f9fa;
-            padding: 10px;
-            border-radius: 10px;
-        }
-        .top-right-menu a {
-            margin: 0 15px;
-            text-decoration: none;
-            font-weight: bold;
-            color: #007bff;
-            font-size: 16px;
-        }
-        .top-right-menu a:hover {
-            color: #0056b3;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# Creating the top-right menu using HTML
-menu_html = """
-    <div class="top-right-menu">
-        <a >Home</a>
-        <a >About</a>
-        <a >Contact</a>
-        <a >Subscribe</a>
-    </div>
-"""
-st.markdown(menu_html, unsafe_allow_html=True)
+st.markdown(
+    """
+    <div class="top-menu">
+        <a href = "#"> HOME </a>
+        <a href = "#"> ABOUT US </a>
+        </div>
+        """, unsafe_allow_html=True
+        )
 
 
 
